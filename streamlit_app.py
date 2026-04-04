8f53d58d558e0d0f925e92413189ee440f51fb13
mahewishs-cloud <mahewish.s@gmail.com>
2026-04-04 09:48:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe
 if ingredients_list:
      ingredients_string=''
      for fruit_chosen in ingredients_list:
-        ingredients_string+=fruit_chosen+' '
+          ingredients_string+=fruit_chosen+' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)  
  
@@ -46,5 +46,5 @@ if ingredients_list:
          session.sql(my_insert_stmt).collect()
          st.success(f'Your Smoothie is ordered'+','+(name_on_order)+'!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+# sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
