80b150791d4e2d13b83ec8e41322ebd6ba4dd0d2
mahewishs-cloud <mahewish.s@gmail.com>
2026-04-04 09:56:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,10 +33,10 @@ if ingredients_list:
 
   
     
-     time_to_insert=st.button('Submit Order')
-     if time_to_insert:
-         session.sql(my_insert_stmt).collect()
-         st.success(f'Your Smoothie is ordered'+','+(name_on_order)+'!', icon="✅")
+       time_to_insert=st.button('Submit Order')
+       if time_to_insert:
+          session.sql(my_insert_stmt).collect()
+          st.success(f'Your Smoothie is ordered'+','+(name_on_order)+'!', icon="✅")
 
 # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 # sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
