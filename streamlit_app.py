f1cd7f3c5a5a62bf4f42e0c305a7176e9b293725
mahewishs-cloud <mahewish.s@gmail.com>
2026-04-04 09:55:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
+
 from snowflake.snowpark.functions import col
 import requests
 
@@ -10,39 +10,31 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
 )
-# option=st.selectbox(
-#     'What is your favorite fruit?',
-#     ('Banana','Strawberries','Peaches'))
-
-# st.write('Your favorite fruit is:',option)
 name_on_order=st.text_input('Name of Smoothie')
 st.write('The name on your Smoothie will be:',name_on_order)
 
-#session = get_active_session()
 cnx=st.connection("snowflake")
 session=cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe
                                ,max_selections=5)
 
 if ingredients_list:
-     ingredients_string=''
-     for fruit_chosen in ingredients_list:
-          ingredients_string+=fruit_chosen+' '
+    ingredients_string=''
+    for fruit_chosen in ingredients_list:
+        ingredients_string+=fruit_chosen+' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)  
  
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
 
-   # st.write(my_insert_stmt) 
-    #st.stop()
+  
     
-    time_to_insert=st.button('Submit Order')
-    if time_to_insert:
+     time_to_insert=st.button('Submit Order')
+     if time_to_insert:
          session.sql(my_insert_stmt).collect()
          st.success(f'Your Smoothie is ordered'+','+(name_on_order)+'!', icon="✅")
 
